c9a81374106903c8e7afbf1e87f76c6f8675f30f
Johanna Roschke <hanni@Johannas-MacBook-Pro.local>
2023-11-06 10:58:38 +0000

    Changed example.py

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
 from main import CloudProcessor
 
-dataset_path = '/Users/hanni/pythonProject6/trade_wind_clouds/data/'
-data = '/Users/hanni/Documents/Masterarbeit/AMT-Paper/clouds/target_classification/20211202_new_cloudnet.nc'
+input_data = 'PATH'
+output_data = 'PATH'
+plot_data = 'PATH'
 
-cloud_processor = CloudProcessor(file_path = data,
-                                 dataset_path = '/Users/hanni/trade_wind_clouds/data/',
-                                 plot_path =  dataset_path)
-cloud_processor.create_cloud_dataset(dataset_path = '/Users/hanni/trade_wind_clouds/data/',
+cloud_processor = CloudProcessor(file_path = input_data,
+                                 dataset_path = output_data,
+                                 plot_path =  plot_data)
+cloud_processor.create_cloud_dataset(dataset_path = output_data,
                                      plot=True)
 
 
